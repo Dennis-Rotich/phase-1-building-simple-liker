2869427e26284dc48264cddf89489b24c5c6d983
Dennis <denkipyator@gmail.com>
2024-01-25 16:47:30 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -3,9 +3,46 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+//Capture HTML elements and remove error modal
+const error = document.getElementById('modal');
+const likeBtn = document.getElementsByClassName('like-glyph');
+error.className = 'hidden';
 
+//Changing like button to fully red
+function updateLikes(btn){
+  btn.textContent = FULL_HEART;
+  btn.className = 'activated-heart';
+  btn.onclick = ()=>{unlike(btn)};
+}
 
+//Changing the like button back to original
+function unlike(btn){
+  btn.textContent = EMPTY_HEART;
+  btn.className = '';
+}
 
+//Displaying error on screen
+function showError(err){
+  error.className = '';
+  error.innerHTML = `<h2>${err}</h2>
+          <p id="modal-message"></p>`
+  setTimeout(()=>{
+    error.className = 'hidden';
+  },3000)
+}
+
+//Add event listener to the like button
+for(let btn of likeBtn){
+  if(typeof btn === 'object'){
+    btn.onclick = ()=>{
+      mimicServerCall()
+      .then(updateLikes(btn))
+      .catch((err)=>{
+        showError(err);
+      })
+    }
+  }
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
